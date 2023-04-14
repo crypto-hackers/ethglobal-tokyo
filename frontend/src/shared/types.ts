77c13a931eab5794fba3c5545375097ecb98d9df
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-04-14 23:13:26 +0900

    add metamask

---
+++ frontend/src/shared/types.ts
@@ -0,0 +1,10 @@
+export type ExternallyOwnedAccount = string;
+
+export type ExternallyOwnedAccounts = Array<ExternallyOwnedAccount>;
+
+export enum ChainId {
+  ethereum = 1,
+  goerli = 5,
+  polygon = 137,
+  mumbai = 80001,
+}
