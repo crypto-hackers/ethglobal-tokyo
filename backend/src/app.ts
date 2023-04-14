258ea37d765c9e30bf999390c841de425b16d352
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-04-14 23:54:52 +0900

    add express

---
+++ backend/src/app.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const err: any = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
+});
+
+export default app;
