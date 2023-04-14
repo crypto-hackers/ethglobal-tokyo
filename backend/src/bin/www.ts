05966c7fd96328c9622c6d5c69e522c97f17ff57
Satoshi Ogura <117020745+o3osatoshi@users.noreply.github.com>
2023-04-15 07:06:22 +0900

    start on server

--- backend/src/bin/www.ts
+++ backend/src/bin/www.ts
@@ -3,7 +3,7 @@
 import app from "../app";
 import http from "http";
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "8000");
 app.set("port", port);
 
 const server = http.createServer(app);
