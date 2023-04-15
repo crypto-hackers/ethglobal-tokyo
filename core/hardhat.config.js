51f23f034092717f74b54745042241394fe30556
Tomoyuki Yamasaki <tomoyukiyamasaki@Tomoyukis-MacBook-Pro.local>
2023-04-16 03:40:42 +0900

    deployed to scroll

--- core/hardhat.config.js
+++ core/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: '../.env' });
-const{MUMBAI_ALCHEMY_KEY, POLYGON_ALCHEMY_KEY,MUMBAI_RPC,POLYGON_RPC,PRIVATE_KEY,POLYGONSCAN_KEY} = process.env;
+const { MUMBAI_ALCHEMY_KEY, POLYGON_ALCHEMY_KEY, MUMBAI_RPC, POLYGON_RPC, PRIVATE_KEY, POLYGONSCAN_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,11 +13,27 @@ module.exports = {
     },
     polygon: {
       url: `${POLYGON_RPC}${POLYGON_ALCHEMY_KEY}`,
-
+      accounts: [PRIVATE_KEY]
+    },
+    scrollAlpha: {
+      url: 'https://alpha-rpc.scroll.io/l2' || '',
       accounts: [PRIVATE_KEY]
     }
   },
-  etherscan:{
-    apiKey : POLYGONSCAN_KEY
+  etherscan: {
+    apiKey: {
+      scrollAlpha: 'abc',
+    },
+    //apiKey : POLYGONSCAN_KEY
+    customChains: [
+      {
+        network: 'scrollAlpha',
+        chainId: 534353,
+        urls: {
+          apiURL: 'https://blockscout.scroll.io/api',
+          browserURL: 'https://blockscout.scroll.io/',
+        },
+      },
+    ]
   }
 };
